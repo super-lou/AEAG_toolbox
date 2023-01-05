4fbec99fb3d87b6777a610f5e1e1fd50523aa1a1
louis.heraut <louis.heraut@inrae.fr>
2023-01-05 15:21:18 +0100

    modify README

--- script_extract.R
+++ script_extract.R
@@ -1,33 +1,25 @@
-# \\\
-# Copyright 2021-2022 Louis Héraut*1,
-#                     Éric Sauquet*2,
-#                     Valentin Mansanarez
+# Copyright 2021-2023 Louis Héraut (louis.heraut@inrae.fr)*1,
+#                     Éric Sauquet (eric.sauquet@inrae.fr)*1
 #
 # *1   INRAE, France
-#      louis.heraut@inrae.fr
-# *2   INRAE, France
-#      eric.sauquet@inrae.fr
 #
-# This file is part of ash R toolbox.
+# This file is part of AEAG_toolbox R toolbox.
 #
-# Ash R toolbox is free software: you can redistribute it and/or
+# AEAG_toolbox R toolbox is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# Ash R toolbox is distributed in the hope that it will be useful, but
+# AEAG_toolbox R toolbox is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ash R toolbox.
+# along with AEAG_toolbox R toolbox.
 # If not, see <https://www.gnu.org/licenses/>.
-# ///
-#
-#
-# R/script_extract.R
-#
+
+
 # Script that manages the call to the right process in order to
 # realise extraction of data.
 
